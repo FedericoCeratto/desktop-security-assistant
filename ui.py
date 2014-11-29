14ce8f79e8261e79e221f7f3c63e54dd560ca409
Federico Ceratto <federico.ceratto@gmail.com>
2014-11-29 17:36:03 +0000

    Add link, code cleanup

--- ui.py
+++ ui.py
@@ -5,20 +5,20 @@
 
 """
 
-# Released under AGPLv3+ license, see LICENSE.txt
+# Released under AGPLv3+ license, see LICENSE
 
 import pygtk
 pygtk.require('2.0')
 import gtk
+
 
 class UI(object):
 
     def hide_check_tab(self, button):
         page = self._notebook.get_current_page()
         self._notebook.remove_page(page)
-        # Need to refresh the widget --
         # This forces the widget to redraw itself.
-        self._notebook.queue_draw_area(0,0,-1,-1)
+        self._notebook.queue_draw_area(0, 0, -1, -1)
 
     def delete(self, widget, event=None):
         gtk.main_quit()
@@ -33,14 +33,14 @@ class UI(object):
         window.set_title("Security assistant")
         window.set_icon_from_file("cat.png")
 
-        table = gtk.Table(3,6,False)
+        table = gtk.Table(3, 6, False)
         window.add(table)
 
         # Create a new notebook, place the position of the tabs
         notebook = gtk.Notebook()
         notebook.show()
         notebook.set_tab_pos(gtk.POS_TOP)
-        table.attach(notebook, 0,6,0,1)
+        table.attach(notebook, 0, 6, 0, 1)
         notebook.set_tab_pos(4)
         notebook.set_current_page(0)
         self._notebook = notebook
@@ -50,7 +50,7 @@ class UI(object):
         button.show()
         button.connect("clicked", self.delete)
         button.set_size_request(10, 20)
-        table.attach(button, 0,1,1,2)
+        table.attach(button, 0, 1, 1, 2)
 
         table.show()
         window.show()
@@ -60,6 +60,14 @@ class UI(object):
         """Add label"""
         label = gtk.Label(text)
         label.set_use_markup(True)
+        label.set_alignment(0, 0.5)
+        label.show()
+        vbox.pack_start(label, False, False, 0)
+
+    @staticmethod
+    def add_url(vbox, url):
+        """Add URL"""
+        label = gtk.LinkButton(url, label='Documentation')
         label.set_alignment(0, 0.5)
         label.show()
         vbox.pack_start(label, False, False, 0)
@@ -87,12 +95,10 @@ class UI(object):
             i.set_from_file('rating.png')
             hbox.pack_start(i, expand=False, fill=False, padding=0)
 
-
-
     def add_check_tab(self, check):
         """Add a tab with the output of a check"""
 
-        frame = gtk.Frame(check.name)
+        frame = gtk.Frame()
         frame.show()
         frame.set_border_width(10)
         frame.set_size_request(150, 15)
@@ -102,9 +108,9 @@ class UI(object):
         frame.add(vbox)
 
         self.add_label(vbox, "%s" % check.desc)
-        self.add_label(vbox, "URL: %s" % check.url)
         self.add_rating(vbox, "risk", check.risk)
         self.add_rating(vbox, "difficulty", check.difficulty)
+        self.add_url(vbox, check.url)
 
         # hide button
         hidebutton = gtk.Button("Hide")
@@ -113,79 +119,7 @@ class UI(object):
         hidebutton.connect('clicked', self.hide_check_tab)
         vbox.pack_start(hidebutton, False, False, 0)
 
-
-        #self._notebook.append_page(vbox, gtk.Label(check.name))
         self._notebook.append_page(frame, gtk.Label(check.name))
-        return
-
-
-
-        frame = gtk.Frame(check.name)
-        frame.set_border_width(10)
-        frame.set_size_request(150, 15)
-        frame.show()
-
-        table1 = gtk.Table(rows=6, columns=1)
-        table1.show()
-        frame.add(table1)
-
-        label = gtk.Label(check.desc)
-        label.show()
-        table1.attach(label,0,1,0,1)
-
-        #entry=gtk.Entry()
-        #table1.attach(entry,1,3,0,1)
-        #entry.show()
-
-        label = gtk.Label("URL: %s" % check.url)
-        label.show()
-        table1.attach(label,0,1,2,3)
-
-        # hide button
-        hidebutton = gtk.Button("Hide")
-        hidebutton.set_size_request(10, 7)
-        hidebutton.connect('clicked', self.hide_check_tab)
-        hidebutton.show()
-
-        table1.attach(hidebutton, 0, 1, 4, 6)
-
-        table1.show()
-        namelabel = gtk.Label(check.name)
-        self._notebook.append_page(frame, namelabel)
-
-        return
-
-
-
-        frame = gtk.Frame(check.name)
-        frame.set_border_width(10)
-        frame.set_size_request(100, 75)
-        frame.show()
-
-        # desc
-        desclabel = gtk.Label(check.desc)
-        desclabel.show()
-        #frame.add(desclabel)
-
-        namelabel = gtk.Label(check.name)
-
-        # hide button
-        hidebutton = gtk.Button("Hide")
-        hidebutton.set_size_request(10, 7)
-        hidebutton.connect('clicked', self.hide_check_tab)
-        hidebutton.show()
-
-        frame.add(hidebutton)
-
-        self._notebook.append_page(frame, namelabel)
-
-
-
-
-
-
 
     def main(self):
         gtk.main()
-
-
